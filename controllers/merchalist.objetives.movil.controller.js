a21021b42bce1be9aadd04c37e6aa79b85e356cf
Juan Campos <juanlad1221@gmail.com>
2022-06-09 08:54:29 -0300

    ultuma actualizacion

--- controllers/merchalist.objetives.movil.controller.js
+++ controllers/merchalist.objetives.movil.controller.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 //models
 const Objetives = require('../schemas/Objetives')
+const Survey = require('../schemas/Survey')
 
 let currentTime = new Date();
 let year = currentTime.getFullYear()
@@ -11,11 +12,11 @@ let mounth = currentTime.getMonth() + 1
 router.post("/list-objetives-movil", async (req, res) => {
     try {
         const { id } = req.body
-
-        let objetives = await Objetives.where({active:true, Merchandising:id, Año:year, Mes:mounth})
         
-        if(objetives){
-            let data = objetives
+        let survey = await Survey.where({active:true, Merchandising:id, Año:year, Mes:mounth})
+        //console.log(survey)
+        if(survey){
+            let data = survey
             res.status(200).json(data)
         }else{
             res.status(404).json({msg:'Data not found...'})
