9b5136fd56a9b124490ca97376371f63c84b07db
Juan Campos <juanlad1221@gmail.com>
2022-07-05 22:33:29 -0300

    ordenamiento

--- controllers/merchalist.movil.controller.js
+++ controllers/merchalist.movil.controller.js
@@ -15,7 +15,7 @@ router.post("/chats-movil", async (req, res) => {
     try {
         const { id, type } = req.body
         
-        let allChats = await Chats.where({Type_user_emisor:type, User_id_emisor:String(id)})
+        let allChats = await Chats.where({Type_user_emisor:type, User_id_emisor:String(id)}).sort({ status: 1 })
         if(allChats ){
             res.status(200).json(allChats)
         }    
