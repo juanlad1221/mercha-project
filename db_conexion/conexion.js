a16b7727566252f623e1765c01f2ddaefcec5a55
Juan Campos <juanlad1221@gmail.com>
2022-07-16 13:20:14 -0300

    cambios en db

--- db_conexion/conexion.js
+++ db_conexion/conexion.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 
 //------CONECTION DB MONGO ATLAS-------------------------------------------------------
-mongoose.connect('mongodb+srv://juanlad1221:yonome1221@clustershool-widaw.mongodb.net/db_survey?retryWrites=true',{useUnifiedTopology: true, useNewUrlParser:true},(err, res)=>{
+/*mongoose.connect('mongodb+srv://juanlad1221:yonome1221@clustershool-widaw.mongodb.net/db_survey?retryWrites=true',{useUnifiedTopology: true, useNewUrlParser:true},(err, res)=>{
 //mongoose.connect('mongodb://localhost/db_survey',{useNewUrlParser: true},(err, res)=>{
+	if(err){console.log('Error: En consulta...',err)};
+	console.log('Data Base ok...');
+})*/
+
+
+mongoose.connect('mongodb://localhost/db_survey',{useNewUrlParser: true},(err, res)=>{
 	if(err){console.log('Error: En consulta...',err)};
 	console.log('Data Base ok...');
 })
