9bc4edeb4c9ffb021feedfb4f8c9648e9f2364cb
Juan Campos <juanlad1221@gmail.com>
2022-07-23 23:54:33 -0300

    cambio de nuevo en bd

--- db_conexion/conexion.js
+++ db_conexion/conexion.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 
 //------CONECTION DB MONGO ATLAS-------------------------------------------------------
-mongoose.connect('mongodb+srv://juanlad1221:yonome1221@clustershool-widaw.mongodb.net/db_survey?retryWrites=true',{useUnifiedTopology: true, useNewUrlParser:true},(err, res)=>{
+/*mongoose.connect('mongodb+srv://juanlad1221:yonome1221@clustershool-widaw.mongodb.net/db_survey?retryWrites=true',{useUnifiedTopology: true, useNewUrlParser:true},(err, res)=>{
 //mongoose.connect('mongodb://localhost/db_survey',{useNewUrlParser: true},(err, res)=>{
 	if(err){console.log('Error: En consulta...',err)};
 	console.log('Data Base ok...');
-})
+})*/
 
 
-/*mongoose.connect('mongodb://164.92.118.90:27017/db_survey',{useNewUrlParser: true},(err, res)=>{
+mongoose.connect('mongodb://164.92.118.90:27017/db_survey',{useNewUrlParser: true},(err, res)=>{
 	if(err){console.log('Error: En consulta...',err)};
 	console.log('Data Base ok...');
-})*/
+})
 
 /*mongoose.connect('mongodb://localhost:27017/db_survey',{useNewUrlParser: true},(err, res)=>{
 	if(err){console.log('Error: En consulta...',err)};
