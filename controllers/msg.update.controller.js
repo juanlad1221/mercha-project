345155d00c4cc39e7c139bc754cad37e97da9137
Juan Campos <juanlad1221@gmail.com>
2022-08-05 21:18:25 -0300

    cambios en api

--- controllers/msg.update.controller.js
+++ controllers/msg.update.controller.js
@@ -11,76 +11,79 @@ const Clients = require('../schemas/Clients')
 
 
 router.put("/msg-update-movil", async (req, res) => {
-    if (req.body) {
-       //console.log(req.body)
-       let data = await Chats.findOne({_id:ObjectId(req.body.dataChat.id_chat)})
-       if(data.status == 'terminado'){
-        console.log('El chat está finalizado...')
-        res.status(400).end
-       }
+  if (req.body) {
+    //console.log(req.body)
+    let data = await Chats.findOne({ _id: ObjectId(req.body.dataChat.id_chat) })
+    if (data.status == 'terminado') {
+      console.log('El chat está finalizado...')
+      res.status(400).end
+    }
 
-       //armo obj a actualizar
-       let obj = {
-        msg:req.body.msg,
-        Date_msg:new Date(req.body.date_msg),
-        name:req.body.name,
-        type:req.body.dataChat.type_origen,
-        //status: req.body.status
-       }
+    //armo obj a actualizar
+    let obj = {
+      msg: req.body.msg,
+      Date_msg: new Date(req.body.date_msg),
+      name: req.body.name,
+      type: req.body.dataChat.type_origen,
+      //status: req.body.status
+    }
 
-       //data.status = req.body.status
-       let result = await data.Mensajes.push(obj)
-       if(result && data){
-        if(req.body.status == 'terminado'){
-          data.status = 'terminado'
-        }else{
-          data.status = 'activo'
+    //data.status = req.body.status
+    let result = await data.Mensajes.push(obj)
+    if (result && data) {
+      if (req.body.status == 'terminado') {
+        data.status = 'terminado'
+      } else {
+        data.status = 'activo'
+      }
+
+
+      let update = await data.save()
+
+      //envio la actualizacion del chat...
+      //let data2 = await Chats.findOne({_id:ObjectId(req.body.dataChat.id_chat)})
+
+      if (update) {
+        let allChats = await Chats.where({})
+        let misChats = personalFilter('User_id_emisor', String(req.body.dataChat.id_origen), 'Type_user_emisor', req.body.dataChat.type_origen, 'User_id_destino', String(req.body.dataChat.id_origen), 'Type_user_destino', req.body.dataChat.type_origen, allChats)
+
+        if (misChats) {
+          res.status(200).json(misChats)
         }
+        //let arr = []
+        //let new_chats = await Chats.where({_id:ObjectId(req.body.dataChat.id_chat)})
+        /*data2.Mensajes.forEach(e => {
 
-        /*if(req.body.status == 'pendiente'){
-          data.status = 'activo'
-        }*/
-        console.log(data)
-        await data.save()
-        
-        //envio la actualizacion del chat...
-        let data2 = await Chats.findOne({_id:ObjectId(req.body.dataChat.id_chat)})
-        if(data2){
-            let arr = []
-            
-            data2.Mensajes.forEach(e => {
+            if(e.type == req.body.dataChat.type_origen){
+                let obj = {
+                    _id: e._id,
+                    text: e.msg,
+                    createdAt: new Date(e.Date_msg),
+                    user: {
+                      _id: 1,
+                      name: e.name
+                    },
+                  }
+                arr.push(obj)
+            }else{
+                let obj = {
+                    _id: e._id,
+                    text: e.msg,
+                    createdAt: new Date(e.Date_msg),
+                    user: {
+                      _id: 2,
+                      name: e.name
+                    },
+                  }
+                arr.push(obj)
+            }
+        })//end*/
+        //res.status(200).json({status:200,chats:arr})
+      }
 
-                if(e.type == req.body.dataChat.type_origen){
-                    let obj = {
-                        _id: e._id,
-                        text: e.msg,
-                        createdAt: new Date(e.Date_msg),
-                        user: {
-                          _id: 1,
-                          name: e.name
-                        },
-                      }
-                    arr.push(obj)
-                }else{
-                    let obj = {
-                        _id: e._id,
-                        text: e.msg,
-                        createdAt: new Date(e.Date_msg),
-                        user: {
-                          _id: 2,
-                          name: e.name
-                        },
-                      }
-                    arr.push(obj)
-                }
-            })//end
-            //console.log(data2.Mensajes)
-            res.status(200).json({status:200,chats:arr})
-        }
-       
-       }
+    }
 
-    }//end if
+  }//end if
 })//end
 
 
