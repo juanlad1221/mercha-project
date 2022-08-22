49b6cddac3b621267945b74118def0bbaa8453df
Juan Campos <juanlad1221@gmail.com>
2022-08-22 09:13:08 -0300

    cambios finales

--- controllers/auth.movil.controller.js
+++ controllers/auth.movil.controller.js
@@ -6,13 +6,14 @@ let jwt = require('jsonwebtoken');
 const Users = require('../schemas/Users')
 
 //const prisma = new PrismaClient()
-
+require("dotenv").config();
 router.post("/auth-movil", async (req, res) => {
 
     try {
         //Extrae datos del body
         const {movil, password} = req.body
         console.log(movil, password)
+
         let Movil = movil.trim()
         let Password = password.trim()
         let user = await Users.findOne({active:true, phone:Movil})
@@ -24,26 +25,32 @@ router.post("/auth-movil", async (req, res) => {
             if(user && resultPass){
                 let obj = {}
                 //genero token
-                let token = jwt.sign(obj, 'mercha1221 ', { expiresIn: '1h' })
+                let token = jwt.sign(obj, 'mercha1221 ', { expiresIn: '365d' })
+              
+                user.token = token
+                let tokenAlmacenado = await user.save()
 
-                if(user.type == 'MERCHA'){
-                    obj.id = user.id
-                    obj.name = user.name
-                    obj.phone = user.phone
-                    obj.type = user.type
-                }
-                if(user.type == 'SELLER'){
-                    obj.id = user.id
-                    obj.name = user.name
-                    obj.phone = user.phone
-                    obj.type = user.type   
-                }
-
-                //agrego token
-                obj.token = token
-                //envio token y datos
-                //console.log(obj)
-                res.status(200).json(obj)
+                if(tokenAlmacenado){
+                    if(user.type == 'MERCHA'){
+                        obj.id = user.id
+                        obj.name = user.name
+                        obj.phone = user.phone
+                        obj.area_job = user.area_job
+                        obj.type = user.type
+                    }
+                    if(user.type == 'SELLER'){
+                        obj.id = user.id
+                        obj.name = user.name
+                        obj.phone = user.phone
+                        obj.area_job = user.area_job
+                        obj.type = user.type   
+                    }
+    
+                    //agrego token
+                    obj.token = token
+                    //envio token y datos
+                    res.status(200).json(obj)
+                }             
             }else{
                 res.status(404).json({msg:'Not Found...'})
             }
